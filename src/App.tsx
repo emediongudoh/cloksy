0ffa79efe5d4b22515ca997456727c5e7fcd2f6c
Emediong Udoh <emediong5135@gmail.com>
2025-09-07 04:31:12 +0100

    add cloksy heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Clock } from "lucide-react";
+
+// Local imports
 import Countdown from "./components/Countdown";
 
 function App() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-8 bg-[#121212]">
-      <h2 className="text-2xl font-medium text-white uppercase sm:text-4xl">
-        Coming soon ...
+    <div className="flex h-screen flex-col items-center justify-center gap-12 bg-[#121212]">
+      <h2 className="flex items-center gap-2 text-2xl font-medium text-yellow-500/80 uppercase sm:text-4xl">
+        <Clock className="size-8" /> Cloksy
       </h2>
       <Countdown targetDate="2025-11-17T00:00:00" />
       <p className="text-sm text-slate-300">
--- src/components/Timeleft.tsx
+++ src/components/Timeleft.tsx
@@ -6,7 +6,7 @@ type TimeleftProps = {
 export default function Timeleft({ value, label }: TimeleftProps) {
   return (
     <div className="flex flex-col items-center gap-4">
-      <div className="relative flex items-center justify-center rounded-lg bg-gradient-to-b from-[#2a2a2a] to-[#1a1a1a] p-4 text-6xl font-bold text-gray-200 shadow-lg before:absolute before:inset-0 before:rounded-lg before:shadow-inner">
+      <div className="relative flex items-center justify-center rounded-lg border border-white/20 bg-gradient-to-b from-[#2a2a2a] to-[#1a1a1a] p-4 text-6xl font-bold text-gray-200 shadow-lg before:absolute before:inset-0 before:rounded-lg before:shadow-inner">
         {/* top/bottom divider */}
         <div className="absolute top-1/2 right-0 left-0 h-2 bg-black/40"></div>
         {value.toString().padStart(2, "0")}
